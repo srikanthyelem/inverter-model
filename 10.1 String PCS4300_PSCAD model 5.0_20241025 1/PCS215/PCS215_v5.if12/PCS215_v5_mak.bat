0f27e0dc47c4527992db22cfa26a8fdd569fc44d
SrikanthYelem <77850203+srikanthyelem@users.noreply.github.com>
2026-07-27 12:13:51 -0400

    Add files via upload

---
+++ 10.1 String PCS4300_PSCAD model 5.0_20241025 1/PCS215/PCS215_v5.if12/PCS215_v5_mak.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\Intel\Composer XE 2013 SP1\bin\ifortvars.bat"  ia32
+pushd "D:\PSCAD_simulink\9 stringPCS_GFL215\PCS215_PSCAD5.0_240820\PCS215\PCS215_v5.if12\"
+nmake -f PCS215_v5.mak
+popd
